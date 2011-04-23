3955a97c445fa30d70dd52b65bcaec66c8044049
Ryan Petrich <rpetrich@gmail.com>
2011-04-22 20:04:30 -0400

    Cleanup handling of sqlite return status

--- untrackerd.c
+++ untrackerd.c
@@ -12,6 +12,17 @@
 
 static time_t last_time;
 
+static inline bool sqlite_is_success(int result)
+{
+	switch (result) {
+		case SQLITE_OK:
+		case SQLITE_DONE:
+			return true;
+		default:
+			return false;
+	}
+}
+
 static inline bool clear_data()
 {
 	struct stat file_stat;
@@ -26,11 +37,13 @@ static inline bool clear_data()
 	sqlite3 *database = NULL;
 	// Open Database
 	int result = sqlite3_open(DATABASE_PATH, &database);
-	if (result == SQLITE_OK) {
+	if (sqlite_is_success(result)) {
 		sqlite3_stmt *statement = NULL;
 		CFAbsoluteTime time = CFAbsoluteTimeGetCurrent() - (60.0 * 30.0);
 		// Ensure secure delete is set
 		result = sqlite3_prepare_v2(database, "PRAGMA secure_delete;", -1, &statement, NULL);
+		if (!sqlite_is_success(result))
+			goto close;
 		int secure_delete = 0;
 		while ((result = sqlite3_step(statement)) == SQLITE_ROW) {
 			secure_delete = sqlite3_column_int(statement, 0);
@@ -39,49 +52,51 @@ static inline bool clear_data()
 		if (secure_delete == 0) {
 			sqlite3_exec(database, "PRAGMA secure_delete = 1;", NULL, NULL, NULL);
 		}
-		if (result != SQLITE_DONE)
+		if (!sqlite_is_success(result))
 			goto close;
 		// Clear Cell data
 		result = sqlite3_prepare_v2(database, "DELETE FROM CellLocation WHERE Timestamp < ?;", -1, &statement, NULL);
-		if (result == SQLITE_OK) {
-			result = sqlite3_bind_double(statement, 1, time);
-			if (result == SQLITE_OK) {
-				do {
-					result = sqlite3_step(statement);
-				} while (result == SQLITE_ROW);
-			}
-			sqlite3_finalize(statement);
+		if (!sqlite_is_success(result))
+			goto close;
+		result = sqlite3_bind_double(statement, 1, time);
+		if (sqlite_is_success(result)) {
+			do {
+				result = sqlite3_step(statement);
+			} while (result == SQLITE_ROW);
 		}
+		sqlite3_finalize(statement);
+		if (!sqlite_is_success(result))
+			goto close;
 		// Clear WiFi Data
-		if (result == SQLITE_DONE) {
-			result = sqlite3_prepare_v2(database, "DELETE FROM WifiLocation WHERE Timestamp < ?;", -1, &statement, NULL);
-			if (result == SQLITE_OK) {
-				result = sqlite3_bind_double(statement, 1, time);
-				if (result == SQLITE_OK) {
-					do {
-						result = sqlite3_step(statement);
-					} while (result == SQLITE_ROW);
-				}
-				sqlite3_finalize(statement);
-			}
-			// Apply vacuuming if necessary
-			if (result == SQLITE_DONE) {
-				result = sqlite3_prepare_v2(database, "PRAGMA auto_vacuum;", -1, &statement, NULL);
-				int auto_vacuum = 0;
-				while ((result = sqlite3_step(statement)) == SQLITE_ROW) {
-					auto_vacuum = sqlite3_column_int(statement, 0);
-				}
-				sqlite3_finalize(statement);
-				if (auto_vacuum == 0) {
-					sqlite3_exec(database, "PRAGMA auto_vacuum = 1; VACUUM;", NULL, NULL, NULL);
-				}
-			}
+		result = sqlite3_prepare_v2(database, "DELETE FROM WifiLocation WHERE Timestamp < ?;", -1, &statement, NULL);
+		if (!sqlite_is_success(result))
+			goto close;
+		result = sqlite3_bind_double(statement, 1, time);
+		if (sqlite_is_success(result)) {
+			do {
+				result = sqlite3_step(statement);
+			} while (result == SQLITE_ROW);
 		}
-		// Close Database
+		sqlite3_finalize(statement);
+		if (!sqlite_is_success(result))
+			goto close;
+		// Apply vacuuming if necessary
+		result = sqlite3_prepare_v2(database, "PRAGMA auto_vacuum;", -1, &statement, NULL);
+		if (!sqlite_is_success(result))
+			goto close;
+		int auto_vacuum = 0;
+		while ((result = sqlite3_step(statement)) == SQLITE_ROW) {
+			auto_vacuum = sqlite3_column_int(statement, 0);
+		}
+		sqlite3_finalize(statement);
+		if (auto_vacuum == 0) {
+			sqlite3_exec(database, "PRAGMA auto_vacuum = 1; VACUUM;", NULL, NULL, NULL);
+		}
 close:
+		// Close Database
 		sqlite3_close(database);
 	}
-	return (result == SQLITE_OK) || (result == SQLITE_DONE);
+	return sqlite_is_success(result);
 }
 
 static void timer_callback(CFRunLoopTimerRef timer, void *info)
